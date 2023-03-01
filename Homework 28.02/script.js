72e4fbf0fd151826677a540f32138082e767b8d0
Yegor2043 <75445220+Yegor2043@users.noreply.github.com>
2023-03-01 21:42:44 +0100

    Add files via upload

---
+++ Homework 28.02/script.js
@@ -0,0 +1,43 @@
+// var arr = {
+//     "ru":["голубой", "красный", "зеленый"],
+//     "en":["blue", "red", "green"],
+// };
+
+// console.log(arr.ru[0]);
+
+// const obj = {
+//     kolya: 200,
+//     vasya: 300,
+//     petya: 400,
+// };
+
+// for(let key in obj) {
+//     console.log(`${key} - зарплата ${obj[key]} долларов`);
+// }
+
+//  const obj = {
+//      js: ["jQuery", "Angular"],
+//      php: "hello", 
+//      css: "world"
+// };
+
+// console.log(`${obj.js[0]}`);
+
+// const obj = {
+//     "ru":["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
+//     "en":["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+// };
+
+// console.log(`${obj.ru[0]} and ${obj.en[2]}`)
+
+const obj = {
+    first: "Monday",
+    second: "Tuesday",
+    third: "Wednesday",
+    fourth: "Thursday",
+    fifth: "Friday",
+    sixth: "Saturday",
+    seventh: "Sunday"
+};
+
+console.log(obj.third);
